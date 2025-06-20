376fd60deab54758a3f9d8ab0df7d77e447db68b
apelsinom <apelsinom@mail.ru>
2025-06-20 16:02:39 +0300

    fix: edit stiles NoNewsPage

--- src/features/news-list/no-news-page.tsx
+++ src/features/news-list/no-news-page.tsx
@@ -1,7 +1,9 @@
 export const NoNewsPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-center mt-32">
-      <h1 className="text-7xl font-bold m-0">BubblyNews has no news, create your first news</h1>
+    <div className="w-full flex flex-col items-center justify-center text-center mt-32">
+      <h1 className="w-[90%] text-2xl lg:text-5xl font-bold">
+        BubblyNews has no news, create your first news
+      </h1>
     </div>
   )
 }
